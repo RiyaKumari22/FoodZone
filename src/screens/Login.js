63e60387ea7e636b623f2e080c47642e80bbdd5b
Riya Kumari <riya.kumari.4rmk@gmail.com>
2024-01-01 14:15:52 +0530

    commit it

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -33,24 +33,8 @@ if(json.success)
       setCredentials({...credentials,[event.target.name]:event.target.value})
   }
   return (
-//     <div className='container'>
-//     <form onSubmit={(handleSubmit)}>
-  
-// <div className="mb-3">
-//   <label for="exampleInputEmail1" className="form-label">Email address</label>
-//   <input type="email" className="form-control" onChange={onChange}  name='email' value={credentials.email} id="exampleInputEmail1" aria-describedby="emailHelp"/>
-  
-// </div>
-// <div className="mb-3">
-//   <label for="exampleInputPassword1" className="form-label">Password</label>
-//   <input type="password" className="form-control" onChange={onChange} name='password' value={credentials.password} id="exampleInputPassword1"/>
-// </div>
 
-
-// <button type="submit" className="btn btn-primary">Submit</button>
-// <Link to="/createuser" className='m-3 btn btn-danger'>SignUp</Link>
-// </form>
-//   </div>
+  
 <div style={{backgroundImage: 'url("https://images.pexels.com/photos/326278/pexels-photo-326278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', height: '100vh', backgroundSize: 'cover' }}>
 <div>
   <Navbar />
